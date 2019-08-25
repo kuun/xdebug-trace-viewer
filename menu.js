cf6fc7f6304664b7f81f9eedc119c7a52de8c25c
kuun <kuunwang@gmail.com>
2019-08-25 17:38:42 +0800

    update help menus

--- menu.js
+++ menu.js
@@ -1,183 +1,175 @@
-'use strict';
+
 const path = require('path');
-const {app, Menu, shell, dialog} = require('electron');
 const {
-	is,
-	appMenu,
-	aboutMenuItem,
-	openUrlMenuItem,
-	openNewGitHubIssue,
-	debugInfo
+  app, Menu, shell, dialog,
+// eslint-disable-next-line import/no-extraneous-dependencies
+} = require('electron');
+const {
+  is,
+  appMenu,
+  aboutMenuItem,
+  openUrlMenuItem,
+  openNewGitHubIssue,
+  debugInfo,
 } = require('electron-util');
 const config = require('./config');
 
 const showPreferences = () => {
-	// Show the app's preferences here
+  // Show the app's preferences here
 };
 
 let mainWindow;
 
 const helpSubmenu = [
-	openUrlMenuItem({
-		label: 'Website',
-		url: 'https://github.com/kuun/xdebug-trace-viwer'
-	}),
-	openUrlMenuItem({
-		label: 'Source Code',
-		url: 'https://github.com/kuun/xdebug-trace-viwer'
-	}),
-	{
-		label: 'Report an Issue…',
-		click() {
-			const body = `
-<!-- Please succinctly describe your issue and steps to reproduce it. -->
+  openUrlMenuItem({
+    label: 'Website',
+    url: 'https://github.com/kuun/xdebug-trace-viewer',
+  }),
+  {
+    label: 'Report an Issue…',
+    click() {
+      const body = `${debugInfo()}`;
 
-
----
-
-${debugInfo()}`;
-
-			openNewGitHubIssue({
-				user: 'kuun',
-				repo: 'xdebug-trace-viwer',
-				body
-			});
-		}
-	}
+      openNewGitHubIssue({
+        user: 'kuun',
+        repo: 'xdebug-trace-viewer',
+        body,
+      });
+    },
+  },
 ];
 
 if (!is.macos) {
-	helpSubmenu.push(
-		{
-			type: 'separator'
-		},
-		aboutMenuItem({
-			icon: path.join(__dirname, 'static', 'icon.png'),
-			text: 'Created by kuun'
-		})
-	);
+  helpSubmenu.push(
+    {
+      type: 'separator',
+    },
+    aboutMenuItem({
+      icon: path.join(__dirname, 'static', 'icon.png'),
+      text: 'A Xdebug trace viewer, created by kuun<kuunwang@gmail.com>',
+    }),
+  );
 }
 
 const debugSubmenu = [
-	{
-		label: 'Show Settings',
-		click() {
-			config.openInEditor();
-		}
-	},
-	{
-		label: 'Show App Data',
-		click() {
-			shell.openItem(app.getPath('userData'));
-		}
-	},
-	{
-		type: 'separator'
-	},
-	{
-		label: 'Delete Settings',
-		click() {
-			config.clear();
-			app.relaunch();
-			app.quit();
-		}
-	},
-	{
-		label: 'Delete App Data',
-		click() {
-			shell.moveItemToTrash(app.getPath('userData'));
-			app.relaunch();
-			app.quit();
-		}
-	}
+  {
+    label: 'Show Settings',
+    click() {
+      config.openInEditor();
+    },
+  },
+  {
+    label: 'Show App Data',
+    click() {
+      shell.openItem(app.getPath('userData'));
+    },
+  },
+  {
+    type: 'separator',
+  },
+  {
+    label: 'Delete Settings',
+    click() {
+      config.clear();
+      app.relaunch();
+      app.quit();
+    },
+  },
+  {
+    label: 'Delete App Data',
+    click() {
+      shell.moveItemToTrash(app.getPath('userData'));
+      app.relaunch();
+      app.quit();
+    },
+  },
 ];
 
 const macosTemplate = [
-	appMenu([
-		{
-			label: 'Preferences…',
-			accelerator: 'Command+,',
-			click() {
-				showPreferences();
-			}
-		}
-	]),
-	{
-		role: 'fileMenu',
-		submenu: [
-			{
-				label: 'Custom'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				role: 'close'
-			}
-		]
-	},
-	{
-		role: 'viewMenu'
-	},
-	{
-		role: 'windowMenu'
-	},
-	{
-		role: 'help',
-		submenu: helpSubmenu
-	}
+  appMenu([
+    {
+      label: 'Preferences…',
+      accelerator: 'Command+,',
+      click() {
+        showPreferences();
+      },
+    },
+  ]),
+  {
+    role: 'fileMenu',
+    submenu: [
+      {
+        label: 'Custom',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        role: 'close',
+      },
+    ],
+  },
+  {
+    role: 'viewMenu',
+  },
+  {
+    role: 'windowMenu',
+  },
+  {
+    role: 'help',
+    submenu: helpSubmenu,
+  },
 ];
 
 function selectTraceFile() {
-	dialog.showOpenDialog(
-		{ properties: ['openFile'] },
-		(fileNames) => {
-			if (fileNames && fileNames.length > 0) {
-				let fileName = fileNames[0];
-				console.log(fileName);
-				mainWindow.webContents.send('open-file', fileName);
-			}
-
-		}
-	);
+  dialog.showOpenDialog(
+    { properties: ['openFile'] },
+    (fileNames) => {
+      if (fileNames && fileNames.length > 0) {
+        const fileName = fileNames[0];
+        console.log(fileName);
+        mainWindow.webContents.send('open-file', fileName);
+      }
+    },
+  );
 }
 
 // Linux and Windows
 const otherTemplate = [
-	{
-		role: 'fileMenu',
-		submenu: [
-			{
-				label: 'Open file',
-				click: () => selectTraceFile()
-			},
-			{
-				type: 'separator'
-			},
-			{
-				role: 'quit'
-			}
-		]
-	},
-	{
-		role: 'viewMenu'
-	},
-	{
-		role: 'help',
-		submenu: helpSubmenu
-	}
+  {
+    role: 'fileMenu',
+    submenu: [
+      {
+        label: 'Open file',
+        click: () => selectTraceFile(),
+      },
+      {
+        type: 'separator',
+      },
+      {
+        role: 'quit',
+      },
+    ],
+  },
+  {
+    role: 'viewMenu',
+  },
+  {
+    role: 'help',
+    submenu: helpSubmenu,
+  },
 ];
 
 const template = process.platform === 'darwin' ? macosTemplate : otherTemplate;
 
 if (is.development) {
-	template.push({
-		label: 'Debug',
-		submenu: debugSubmenu
-	});
+  template.push({
+    label: 'Debug',
+    submenu: debugSubmenu,
+  });
 }
 
 module.exports = {
-	menu: Menu.buildFromTemplate(template),
-	setMainWindow: (window) => mainWindow = window
+  menu: Menu.buildFromTemplate(template),
+  setMainWindow: (window) => mainWindow = window,
 };
